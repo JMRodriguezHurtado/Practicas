dada1b9e1adcac84a7de470bbf25d6a94e8bac0d
JMRodriguezHurtado <jmrodhurtado1989@hotmail.com>
2023-08-07 12:17:49 -0600

    Create TragosEnElBar.js
    usando una queue para almacenar cantidades

---
+++ TragosEnElBar.js
@@ -0,0 +1,45 @@
+function Queue () {
+    this.lista = [];
+  }
+  Queue.prototype.enqueue = function(valor) {
+    return this.lista.push(valor);
+  }
+  Queue.prototype.dequeue = function() {
+    if(!this.lista.length) return undefined;
+    return this.lista.shift();
+  }
+  Queue.prototype.size = function() {
+    return this.lista.length;
+  }
+  Queue.prototype.search = function(valor) {
+    return this.lista.includes(valor); 
+  }
+
+  function contarTragos (Queue) {
+    const tragos = {};
+    if (Queue.size() === 0) return "No hay tragos";
+    while(Queue.size() > 0) {
+        const trago = Queue.dequeue();
+       if (tragos.hasOwnProperty(trago)) {
+        tragos[trago].cantidad++;
+       } else {
+        tragos[trago] = {trago: trago, cantidad: 1};
+       }
+    } return Object.values(tragos);  
+}
+
+let cuenta = new Queue;
+
+cuenta.enqueue("margarita");
+cuenta.enqueue("tequila sunshine");
+cuenta.enqueue("vodka");
+cuenta.enqueue("mojito");
+cuenta.enqueue("margarita");
+cuenta.enqueue("fantasma");
+cuenta.enqueue("tequila sunshine");
+cuenta.enqueue("vino");
+
+console.log(cuenta);
+
+console.log(contarTragos(cuenta));
+
